e01bf232efe3094ba66e3d0ea051e4bb53377d01
Alex Moreira <alex.moreira.andrade@hotmail.com>
2022-12-18 14:53:19 -0300

    Feat: Entities, repositorys, modules, services DB

---
+++ src/infra/database/database.module.ts
@@ -0,0 +1,18 @@
+import { PrismaNotificationsRepository } from './prisma/repositories/prisma-notifications-repository';
+import { PrismaService } from './prisma/prisma.service';
+import { Module } from '@nestjs/common';
+import { NotificationsRepository } from 'src/application/repositories/notifications-repository';
+
+@Module({
+  providers: [
+    PrismaService,
+    {
+      // Sempre que chamarem a inteface ou contrato do repository se devolve o repository
+      provide: NotificationsRepository,
+      useClass: PrismaNotificationsRepository,
+    },
+  ],
+  // Quais providers devem ser compartilhados com quem importar esse modulo, no caso o http module
+  exports: [NotificationsRepository],
+})
+export class DataBaseModule {}
