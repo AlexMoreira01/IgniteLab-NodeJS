809eb39b7b04e27987559308ebea9a2b3c8f449b
Alex Moreira <alex.moreira.andrade@hotmail.com>
2022-12-18 14:50:40 -0300

    Feat: Application main and httop modules

---
+++ src/infra/http/http.module.ts
@@ -0,0 +1,13 @@
+import { SendNotification } from './../../application/use-cases/send-notification';
+import { NotificationsController } from './controllers/notifications.controller';
+import { DataBaseModule } from '../database/database.module';
+import { Module } from '@nestjs/common';
+
+@Module({
+  //  O que importar os serviços do banco como os repository esta no Modulo de database e se importa ele aqui e também no app.module
+  imports: [DataBaseModule],
+  controllers: [NotificationsController],
+  // Para se utilizar o send notification deve se usar o provider pois se nao a injeção de dependencia nao funciona para os que estajam fora dele
+  providers: [SendNotification],
+})
+export class HttpModule {}
