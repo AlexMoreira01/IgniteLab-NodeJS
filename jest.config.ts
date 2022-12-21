1b1cd1ee8d041d81ae60277d92921dee424272a3
Alex Moreira <alex.moreira.andrade@hotmail.com>
2022-12-20 22:21:01 -0300

    Feature: Test to cancel a notification

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,21 @@
-export default {
+import { Config } from 'jest';
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json'; // Para se importar json deve se habilitar no tsconfig.json
+
+const config: Config = {
   moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: 'src',
   testRegex: '.*\\.spec\\.ts$',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
+  // Para entender as importações com alias
   collectCoverageFrom: ['**/*.(t|j)s'],
   coverageDirectory: '../coverage',
   testEnvironment: 'node',
+
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
 };
+
+export default config;
