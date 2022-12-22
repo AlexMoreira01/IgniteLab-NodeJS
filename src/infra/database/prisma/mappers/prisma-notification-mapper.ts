9547314ba4d2199cfc9dc8dd9069ca04a45cf661
Alex Moreira <alex.moreira.andrade@hotmail.com>
2022-12-21 23:33:01 -0300

    Feat: Create Mapper toDomain notification

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,4 +1,6 @@
+import { Notification as RawNotification } from '@prisma/client';
 import { Notification } from '@application/entities/notification';
+import { Content } from '@application/entities/content';
 
 export class PrismaNotificationMapper {
   // Com metodos estaticos não é preciso instanciar a classe
@@ -11,5 +13,23 @@ export class PrismaNotificationMapper {
       readAt: notification.readAt,
       createdAt: notification.createdAt,
     };
+  }
+
+  // Converter do prisma para a camada de dominio da aplicação
+  static toDomain(raw: RawNotification): Notification {
+    // Por mais que se esteja dando um new Notification, nao esta criando uma nova notification
+    // quer dizer que estamos criando uma referencia de uma notificação
+    return new Notification(
+      // Dados passados para o constructor
+      {
+        category: raw.category,
+        content: new Content(raw.content),
+        recipientId: raw.recipientId,
+        readAt: raw.readAt,
+        canceledAt: raw.canceledAt,
+        createdAt: raw.createdAt,
+      },
+      raw.id,
+    );
   }
 }
